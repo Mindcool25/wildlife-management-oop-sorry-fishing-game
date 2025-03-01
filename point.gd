4eb69378ecbe0d71021c45d25a81f328c2158393
mindcool24 <thezachm24@gmail.com>
2025-03-01 06:37:38 -0700

    mor estuffff

---
+++ point.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+var points: int = 0
+
+func setup_point(point_value: int):
+	points = point_value
+	pass
+
+func _ready() -> void:
+	# Select random rotation
+	var tween = create_tween()
+	var new_rot = randfn(0, 2*PI)
+	# Set position and rotation according to the random rotation
+	self.position = Vector2(cos(new_rot)*300, sin(new_rot) * 300)
+	self.rotation = new_rot - (PI/6)
+	
+	# Create tween between an execute it
+	while not tween:
+		pass
+	tween.tween_property(self, "position", Vector2(0,0), 0.72)
+	tween.tween_callback(_ready)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- scripts/fishing.gd
+++ scripts/fishing.gd
@@ -1,27 +1,22 @@
 extends Node2D
 
 @export var circle: Sprite2D
-@export var point: Sprite2D
 @export var player: Sprite2D
+@export var p_num: int
 
 var target
 var start 
-var tween
 var player_rotation = 0
+var curr_points = 0
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	## Start RNG
+	# Start RNG
 	randomize()
 	
 	# Set up positions/targets
 	while not circle:
 		pass
 	target = circle.position
-	while not point:
-		pass
-	point.position = Vector2(cos(0) * 300, sin(0) * 300)
-	point.rotation = -PI/6
 
 func _process(delta: float) -> void:
 	handle_player(delta)
@@ -30,10 +25,17 @@ func _process(delta: float) -> void:
 
 func handle_player(delta: float) -> void:
 	# Rotate right/left
-	if Input.is_action_pressed("ui_right"):
-		player_rotation += delta * 8
-	if Input.is_action_pressed("ui_left"):
-		player_rotation -= delta * 8
+	if p_num == 0:
+		if Input.is_action_pressed("p1 right"):
+			player_rotation += delta * 8
+		if Input.is_action_pressed("p1 left"):
+			player_rotation -= delta * 8
+	elif p_num == 1:
+		if Input.is_action_pressed("p2 right"):
+			player_rotation += delta * 8
+		if Input.is_action_pressed("p2 left"):
+			player_rotation -= delta * 8
+		
 	if Input.is_action_just_pressed("ui_down"):
 		reset_point()
 	
@@ -41,21 +43,23 @@ func handle_player(delta: float) -> void:
 	player.rotation = player_rotation
 	player.position = Vector2(cos(player_rotation) * 55, sin(player_rotation) * 55)
 
+
+var scene = preload("res://point.tscn")
+
 func reset_point() -> void:
-	# Yeet the last tween
-	if tween:
-		tween.stop()
-	# Select random rotation
-	var new_rot = randfn(0, 2*PI)
-	# Set position and rotation according to the random rotation
-	point.position = Vector2(cos(new_rot)*300, sin(new_rot) * 300)
-	point.rotation = new_rot - (PI/6)
-	
-	# Create tween between an execute it
-	tween = create_tween()
-	tween.tween_property(point, "position", Vector2(0,0), 0.72)
+	# Create an instance of the point and add it as a child to the middle circle
+	var instance = scene.instantiate()
+	instance.setup_point(1)
+	$Middle.add_child(instance)
+	pass
 
 func _on_player_area_area_entered(area: Area2D) -> void:
-	print("Player Area Entered")
+	# Get points from point scene
+	var got_points = area.get_parent().get_parent().points
+	# Kill the bitch
+	area.get_parent().get_parent().queue_free()
 	reset_point()
+	# Add points to current score
+	curr_points += got_points
+	print(curr_points)
 	pass # Replace with function body.
